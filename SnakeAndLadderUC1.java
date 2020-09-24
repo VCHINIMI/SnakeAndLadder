d1b6f6d78389521d149bb7eb96c1aba0681de571
VCHINIMI <vinaychinimilli@gmail.com>
2020-09-24 07:29:59 +0530

    [VINAY]Added: SnakeAndLadderUC1

---
+++ SnakeAndLadderUC1.java
@@ -0,0 +1,11 @@
+public class SnakeAndLadderUC1 {
+
+	public static void main(String[] args) {
+		int position = 0;
+		int roll = 0;
+		int rolls_till_now = 0;
+		int option =0;
+		/*          Option 1 = No play  |  option 2 = Ladder  |   option 3 = Snake      */
+		System.out.println("Initial position = 0");
+	}
+}
